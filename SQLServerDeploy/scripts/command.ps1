2b6f350fad2e1525301a45610829c226932c16d6
GustavoAmerico <gustavo.americo@live.com>
2017-07-15 22:37:11 -0300

    acertando  a variavel de entrada e o tempo de execução do script

--- SQLServerDeploy/scripts/command.ps1
+++ SQLServerDeploy/scripts/command.ps1
@@ -1,59 +1,87 @@
 ﻿[CmdletBinding(DefaultParameterSetName = 'None')]
 param(
 
-    [String] [Parameter(Mandatory = $true)]
+    [String] [Parameter(Mandatory = $True)]
     $dacpacPattern,
 
-    [String] [Parameter(Mandatory = $true)]
+    [String] [Parameter(Mandatory = $True)]
     $dacpacPath,
 
-    [String] [Parameter(Mandatory = $true)]
+    [String] [Parameter(Mandatory = $True)]
     $connectionString,
 
-    [String] [Parameter(Mandatory = $true)]
+    [String] [Parameter(Mandatory = $True)]
     $dbName,
 
-    [bool] [Parameter(Mandatory = $false)]
-    $blockOnPossibleDataLoss = $false,
+    [String] [Parameter(Mandatory = $False)]
+    $blockOnPossibleDataLoss = "false",
 
-    [bool] [Parameter(Mandatory = $false)]
-    $verifyDeployment = $true,
+    [String] [Parameter(Mandatory = $False)]
+    $verifyDeployment = "true",
 
-    [bool] [Parameter(Mandatory = $false)]
-    $compareUsingTargetCollation = $true,
+    [String] [Parameter(Mandatory = $False)]
+    $compareUsingTargetCollation = "true",
 
-    [bool] [Parameter(Mandatory = $false)]
-    $allowIncompatiblePlatform = $true
+    [String] [Parameter(Mandatory = $False)]
+    $allowIncompatiblePlatform = "true"
 )
-if(![System.IO.Directory]::Exists($dacpacPath)){
-    Write-Host "Não foi encontrado um diretorio:" $dacpacPath;
-    return;
-}
 
-$fileName = $dacpacPath + "\" + $dacpacPattern 
-Write-Host "Tentando encontrar o arquivo:" $fileName
-try {
- 
-    $file = Get-ChildItem $fileName -Recurse  
-    if ($file.Length = 0) {
-        Write-Host "Não foi encontrado nenhum arquivo"
+$file = function LoadFile() {
+
+    if (![System.IO.Directory]::Exists($dacpacPath)) {
+        Write-Host "Não foi encontrado um diretorio:" $dacpacPath;
         return;
-    }   
-}
-catch {
-    Write-Host "Ocorreu um erro ao carregar os arquivos";
-    return;
+    }
+
+    $fileName = ($dacpacPath + "\" + $dacpacPattern).Trim(); 
+    Write-Host "Tentando encontrar o arquivo:" $fileName
+    try {
+ 
+        $file = Get-ChildItem $fileName -Recurse  
+        if ($file.Length -eq 0) {
+            Throw "Não foi encontrado nenhum arquivo"
+        }
+        else {
+            Write-Host "Arquivo encontrado: " $file
+            return $file;
+        }
+    }
+    catch {
+        Write-Host "Ocorreu um erro ao carregar os arquivos";
+        Throw;
+    }
+
 }
 
-Write-Host "Arquivo encontrado: " $file
-$dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($file);
-Write-Host "Conseguiu carregar o dacpac"
-$dacService = new-object Microsoft.SqlServer.Dac.DacServices($connectionString);
-Write-Host "Conseguiu se conectar ao servidor"
-$option = new-object Microsoft.SqlServer.Dac.DacDeployOptions 
-$option.BlockOnPossibleDataLoss = $blockOnPossibleDataLoss;
-$option.CompareUsingTargetCollation = $compareUsingTargetCollation;
-$option.AllowIncompatiblePlatform = $allowIncompatiblePlatform;
-$option.VerifyDeployment = $verifyDeployment;
-$dacService.Deploy($dp, $dbName, "True", $option);
-Write-Host "Concluiu o deploy"
+function Deploy() {
+
+    $dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($file);
+    Write-Host "Conseguiu carregar o dacpac"
+    $dacService = new-object Microsoft.SqlServer.Dac.DacServices($connectionString);
+    Write-Host "Conseguiu se conectar ao servidor" 
+
+    Write-Host "Preparando as variaveis de publicação"
+    Write-Host "BlockOnPossibleDataLoss: " $blockOnPossibleDataLoss
+    Write-Host "CompareUsingTargetCollation: " $compareUsingTargetCollation
+    Write-Host "AllowIncompatiblePlatform: " $allowIncompatiblePlatform
+    Write-Host "VerifyDeployment: " $verifyDeployment
+
+    $option = new-object Microsoft.SqlServer.Dac.DacDeployOptions 
+
+    $option.BlockOnPossibleDataLoss = [System.Convert]::ToBoolean($blockOnPossibleDataLoss.Trim());
+    $option.CompareUsingTargetCollation = [System.Convert]::ToBoolean( $compareUsingTargetCollation.Trim());
+    $option.AllowIncompatiblePlatform = [System.Convert]::ToBoolean( $allowIncompatiblePlatform.Trim());
+    $option.VerifyDeployment = [System.Convert]::ToBoolean($verifyDeployment.Trim());
+    $option.CommandTimeout = 7200;
+    $dacService.Deploy($dp, $dbName, "True", $option)
+    Write-Host "Concluiu o deploy"
+}
+ 
+#Load Microsoft.SqlServer.Dac assembly
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Dac")
+
+Write-Host "Iniciando o deploy"
+Start-Job Deploy -WarningAction Continue 
+Write-Host "Terminando a execução"
+
+

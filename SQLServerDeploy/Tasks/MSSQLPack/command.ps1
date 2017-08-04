cee0be64743008d31f7df482f07e2034048ca4bf
GustavoAmerico <gustavo.americo@live.com>
2017-08-04 14:22:57 -0300

    Adicionando tarefa para empacotar o projeto

---
+++ SQLServerDeploy/Tasks/MSSQLPack/command.ps1
@@ -0,0 +1,17 @@
+﻿[CmdletBinding(DefaultParameterSetName = 'None')]
+param(
+    [String] [Parameter(Mandatory = $False)] [string]
+    $output = ""
+)
+New-Item -ItemType Directory -Force -Path $output
+$msbuild = @{ 
+    performanceParameters = "/nologo", "/noconsolelogger", "/p:WarningLevel=0", "/clp:ErrorsOnly", "/m:1"
+    loggingParameters     = "/l:FileLogger,Microsoft.Build.Engine;logfile=$output\log.txt"
+    packageParameters     = , "/property:outdir=$output", "/p:configuration=release"
+    targets               = "/t:rebuild"
+}
+MSBuild  `
+    $msbuild.performanceParameters `
+    $msbuild.packageParameters `
+    $msbuild.loggingParameters `
+    $msbuild.targets

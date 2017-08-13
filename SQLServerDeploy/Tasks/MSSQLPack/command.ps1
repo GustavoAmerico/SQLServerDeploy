2f6905efc66612001e5514b89f9f1b3d4e316b64
GustavoAmerico <gustavo.americo@live.com>
2017-08-13 18:59:26 -0300

    Adicionando suporte ao msbuild

--- SQLServerDeploy/Tasks/MSSQLPack/command.ps1
+++ SQLServerDeploy/Tasks/MSSQLPack/command.ps1
@@ -1,16 +1,39 @@
 ﻿[CmdletBinding(DefaultParameterSetName = 'None')]
 param(
-    [String] [Parameter(Mandatory = $False)] [string]
-    $output = ""
+    [String] [Parameter(Mandatory = $True)] [string]
+    $filePattern ,
+    [String] [Parameter(Mandatory = $True)] [string]
+    $output 
 )
 New-Item -ItemType Directory -Force -Path $output
+$path = [environment]::GetEnvironmentVariable("windir","Process")+"\Microsoft.NET\Framework64\v4.0.30319";
+add-type -path $path
+
+
+$fileName = ($filePattern).Trim(); 
+Write-Host "Searching for:" $fileName
+try {
+ 
+    $file = Get-ChildItem $fileName -Recurse  
+    if ($file.Length -eq 0) {
+        Throw "No files found"
+    }
+    else {
+        Write-Host "Found file: " $file
+    }
+}
+catch {
+    Write-Host "There was an error loading the file";
+    Throw;
+}
+
 $msbuild = @{ 
     performanceParameters = "/nologo", "/p:WarningLevel=4", "/clp:Summary", "/m:1"
     loggingParameters     = "/l:FileLogger,Microsoft.Build.Engine;logfile=$output\logdb.txt"
     packageParameters     = , "/property:outdir=$output", "/p:configuration=release"
     targets               = "/t:rebuild"
 }
-MSBuild  `
+MSBuild $fileName `
     $msbuild.performanceParameters `
     $msbuild.packageParameters `
     $msbuild.loggingParameters `
